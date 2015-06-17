90d07d03a8ad83ebae4a9cb9959baa9c4407392c
Kyle Bradshaw <kyle@somedirection.com>
2015-06-17 00:27:39 -0400

    wiring up

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -3,12 +3,74 @@
 
     angular
         .module('app.home')
-        .controller('HomeController', HomeController);
+        .controller('HomeController', HomeController)
+        .factory('TwitterService', TwitterService)
+        .constant('API_ROOT', 'http://localhost:3000');
 
     HomeController.$inject = ['$scope'];
+    TwitterService.$inject = ['$http', '$q', 'API_ROOT'];
 
-    function HomeController($scope) {
+    function HomeController($scope, TwitterService) {
         $scope.pageClass = "home";
-        $scope.content = "What an epic homepage!";
+        $scope.content = "";
+
+        $scope.getTrends = function(){
+          TwitterService.getTrends()
+            .then(function(data){
+              $scope.trends = JSON.parse(data.result.trendData);
+              console.log($scope.trends);
+            })
+            .catch(function(error){
+              console.error('problems', error);
+              $scope.twitterErrors = error.error;
+            })
+        };
+
+        $scope.getUser = function(username){
+          console.log("username entered ", username);
+          TwitterService.getUser(username)
+              .then(function(data){
+                  $scope.twitterErrors = undefined;
+                  $scope.results = JSON.parse(data.result.userData);
+              })
+              .catch(function(error){
+                  console.error('there was an error retrieving data: ', error);
+                  $scope.twitterErrors = error.error;
+              })
+        };
     }
+
+    function TwitterService($http, $q, API_ROOT) {
+
+      var getTrends = function(){
+        var d = $q.defer();
+        $http.get(API_ROOT + '/twitter/trends')
+          .success(function(data){
+            return d.resolve(data);
+          })
+          .error(function(error){
+            return d.reject(error);
+          });
+        return d.promise;
+      };
+
+      var getUser = function(username){
+        var d = $q.defer();
+        $http.post(API_ROOT + '/twitter/user', {username : username})
+          .success(function(data){
+            return d.resolve(data);
+          })
+          .error(function(error){
+            return d.reject(error);
+          });
+        return d.promise;
+      };
+
+      return {
+        getTrends : getTrends,
+        getUser : getUser
+      }
+    }
+
+
 }());
